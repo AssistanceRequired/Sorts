b61eb11a4eab76ae644dc62529801482a6ec6455
Jack <Jack@25.237.31.90>
2017-02-06 23:18:01 -0500

    Bubble sort

--- Sorts/src/Sorting.java
+++ Sorts/src/Sorting.java
@@ -174,4 +174,23 @@ public class Sorting {
 	      //returns low so that it does not recurse infinitely over the original pivot.
 	}
 	
+	/**
+	 * Implements bubblesort
+	 * @param arr - The array to be sorted
+	 * @return - A sorted array
+	 */
+	public int[] bubbleSort(int[] arr) {
+
+		boolean swaps = true;
+		while(swaps) {
+			swaps = false;
+			for(int i = 0; i < arr.length - 1; i++) {
+				if(arr[i] > arr[i+1]) {
+					swap(arr, i, i + 1);
+					swaps = true;
+				}
+			}
+		}
+		return arr;
+	}
 }
